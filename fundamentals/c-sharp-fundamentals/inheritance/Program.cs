abe7d7b3490e48afd0408aee99dc1ff7f17394eb
level2fast@gmail.com <level2fast@gmail.com>
2022-09-05 21:51:50 -0700

    Example: inheritance
    Example: inheritance

---
+++ fundamentals/c-sharp-fundamentals/inheritance/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+namespace inheritance
+{
+    public class Honda
+    {
+        public string Engine { get; set; }
+        public string Transmission { get; set; }
+
+        public Honda()
+        {
+
+        }
+
+    }
+    public class Civic : Honda
+    {
+        public Civic()
+        {
+        }
+    }
+    public class EntryPoint
+    {
+        static void Main(string[] args)
+        {
+            // Example: inheritance
+            // The civic class has access to the Honda
+            // class properties.
+            var newCar = new Civic();
+            newCar.Engine = "V-Tech";
+     
+            // Example Composition
+        }
+    }
+
+}
