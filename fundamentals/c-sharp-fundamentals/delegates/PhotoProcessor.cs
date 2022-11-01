9ad64e87ce21e29466b37f6026c1579383d5dc37
level2fast@gmail.com <level2fast@gmail.com>
2022-10-31 21:04:57 -0700

    Delegate fundamentals
    Delegate fundamentals

---
+++ fundamentals/c-sharp-fundamentals/delegates/PhotoProcessor.cs
@@ -0,0 +1,78 @@
+﻿namespace Delegates
+{
+    public class Photo
+    {
+        static public void Save() { }
+        static public Photo Load(string path) { return new Photo(); }
+
+    }
+    public class PhotoProcessor
+    {
+        /// <summary>
+        /// parameter to delegat can hold a pointer to a function
+        /// or a group of functions that have the signatre
+        /// </summary>
+        /// <param name="photo"></param>
+        public delegate void PhotoFilterHandler(Photo photo);
+        
+        /// <summary>
+        /// Process method to process a photo
+        /// </summary>
+        /// <param name="path"></param>
+        public void Process(string path, PhotoFilterHandler filterHandler)
+        {
+            Photo photo = Photo.Load(path);
+            var filters = new PhotoFilters();
+
+            // We can remove the logic below
+            // to make this method extensible 
+            // and pass a delegate instead.
+            // filters.ApplyBrightness(photo);
+            // filters.ApplyContrast(photo);
+            // filters.Resize(photo);  
+
+            // Using the delegate means that
+            // this code does not know what type
+            // of filter will be applied. It's up
+            // to the client code to decided. Doing
+            // this means that this framework does not have 
+            // to be complied or redeployed which makes it 
+            // extensible
+            filterHandler(photo);
+        }
+
+        /// <summary>
+        /// Process a photo, but this time use built in
+        /// delegates provided by .NET
+        /// </summary>
+        /// <param name="path"></param>
+        /// <param name="filterHandler"></param>
+        public void Process(string path, Action<Photo> filterHandler)
+        {
+            // built in delegates provided my .NET
+            // System.Action<>
+            // System.Func<>
+
+            Photo photo = Photo.Load(path);
+            var filters = new PhotoFilters();
+
+            // We can remove the logic below
+            // to make this method extensible 
+            // and pass a delegate instead.
+            // filters.ApplyBrightness(photo);
+            // filters.ApplyContrast(photo);
+            // filters.Resize(photo);  
+
+            // Using the delegate means that
+            // this code does not know what type
+            // of filter will be applied. It's up
+            // to the client code to decided. Doing
+            // this means that this framework does not have 
+            // to be complied or redeployed which makes it 
+            // extensible
+            filterHandler(photo);
+        }
+    }
+}
+
+
--- fundamentals/c-sharp-fundamentals/delegates/Program.cs
+++ fundamentals/c-sharp-fundamentals/delegates/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Delegates
+{
+    /// <summary>
+    /// A Delegate is an objecdt that knows how to call
+    /// a method ( or a group of methods). Could also 
+    /// be a reference to a function
+    /// 
+    /// Delegate are used when designing frameworks to 
+    /// provide extensibility
+    /// 
+    /// Use a delegate when:
+    ///     - an eventing design pattern is used
+    ///     - The caller doesn't need to access other
+    ///       properties or methods on the object 
+    ///       implementing the method.
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var processor = new PhotoProcessor();
+            var filters = new PhotoFilters();
+            
+            // Use custom delegate to process a photo
+            PhotoProcessor.PhotoFilterHandler filterHandler = filters.ApplyBrightness;
+            filterHandler += filters.ApplyContrast;
+            filterHandler += RemoveRedEyeFilter;
+            processor.Process("photo.jpg", filterHandler);
+
+            // Use system delegate to process a photo
+            Action<Photo> filterHandler2 = filters.ApplyBrightness;
+            filterHandler2 += filters.ApplyContrast;
+            filterHandler2 += RemoveRedEyeFilter;
+            processor.Process("photo.jpg", filterHandler2);
+        }
+        static void RemoveRedEyeFilter(Photo photo)
+        {
+            Console.WriteLine("Apply remove red eye");
+        }
+    }
+}
+
+
